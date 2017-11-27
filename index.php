fb97002fcae889e6b0e9b54ef4d03983caed1f5e
Ana Asins <aasins@tsiberia.es>
2017-11-27 16:44:38 +0100

    todo

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/* Incluir la libreria PHPExcel */
+require_once './Classes/PHPExcel.php';
+
+// Crea un nuevo objeto PHPExcel
+$objPHPExcel = new PHPExcel();
+
+// Establecer propiedades
+$objPHPExcel->getProperties()
+->setCreator("VCatala")
+->setLastModifiedBy("Vcatala")
+->setTitle("Documento Excel de Prueba")
+->setSubject("Documento Excel de Prueba")
+->setDescription("Demostracion sobre como crear y abrir archivos de Excel desde PHP.")
+->setKeywords("Excel Office 2007 openxml php")
+->setCategory("Pruebas de Excel");
+
+// Agregar Informacion
+$objPHPExcel->setActiveSheetIndex(0)
+->setCellValue('A1', 'Ana aço es una prova del codi funcionant')
+->setCellValue('A2', 'Valor 1')
+->setCellValue('B2', 'Valor 2')
+->setCellValue('C2', 'Total')
+->setCellValue('A3', '10')
+->setCellValue('B3', '7')
+->setCellValue('C3', '=sum(A3:B3)');
+
+// Renombrar Hoja
+$objPHPExcel->getActiveSheet()->setTitle('Prueba TSI');
+
+// Establecer la hoja activa, para que cuando se abra el documento se muestre primero.
+$objPHPExcel->setActiveSheetIndex(0);
+
+// Se modifican los encabezados del HTTP para indicar que se envia un archivo de Excel.
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="pruebaReal.xlsx"');
+header('Cache-Control: max-age=0');
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save('php://output');
+exit;
+?>
